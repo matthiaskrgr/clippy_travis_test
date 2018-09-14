b10ae662025462a5756f1be19d522ec420ce920b
Matthias Krüger <matthias.krueger@famsik.de>
2018-09-14 11:15:48 +0200

    change code sample

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-// enable additional clippy warnings
-#![cfg_attr(feature = "cargo-clippy", warn(int_plus_one))]
-#![cfg_attr(feature = "cargo-clippy", warn(shadow_reuse, shadow_same, shadow_unrelated))]
-#![cfg_attr(feature = "cargo-clippy", warn(mut_mut))]
-#![cfg_attr(feature = "cargo-clippy", warn(nonminimal_bool))]
-#![cfg_attr(feature = "cargo-clippy", warn(pub_enum_variant_names))]
-#![cfg_attr(feature = "cargo-clippy", warn(range_plus_one))]
-#![cfg_attr(feature = "cargo-clippy", warn(string_add, string_add_assign))]
-#![cfg_attr(feature = "cargo-clippy", warn(stutter))]
-//#![cfg_attr(feature = "cargo-clippy", warn(result_unwrap_used))]
-#![cfg_attr(feature = "cargo-clippy", deny(clippy))]
-
-
 fn main() {
-    println!("Hello, world!");
-   let unused = 4;
-   if (unused > 1) == true  { println!("yay"); }
-
+	let x = format!("{}", "hello world");
+	println!("{}", x);
 }
 
